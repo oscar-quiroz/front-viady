bf86068cd7f92c07a15bb6166da183ee3f23d0e6
oscar-quiroz <kairox.rt@gmail.com>
2021-02-09 11:43:56 -0500

    navbar terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PrincipalModule } from './principal/principal.module';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrincipalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
