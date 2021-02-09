dbef660511a1e5dd13d33f79e6225bcf58488dce
oscar-quiroz <kairox.rt@gmail.com>
2021-02-09 13:16:08 -0500

    imagenes

--- src/app/principal/principal.module.ts
+++ src/app/principal/principal.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router'
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { ImagesComponent } from './images/images.component';
 
 
 
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, ImagesComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports:[
-    NavbarComponent
+    NavbarComponent,
+    ImagesComponent
   ]
 })
 export class PrincipalModule { }
